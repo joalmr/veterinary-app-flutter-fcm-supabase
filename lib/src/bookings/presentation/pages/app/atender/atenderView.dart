147ab72e3c0636e1527bb0c6e204e5051d4ae8e3
Alonso <joalmr@gmail.com>
2021-04-28 21:27:55 -0500

    booking

---
+++ lib/src/bookings/presentation/pages/app/atender/atenderView.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AtenderView extends StatelessWidget {
+  const AtenderView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Atender'),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
